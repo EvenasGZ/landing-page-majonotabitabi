c811a9bdf76f10abd2defb71b1fb9320f99811d0
Rudolf <119478278+EvenasGZ@users.noreply.github.com>
2025-03-10 18:17:28 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ document.addEventListener("DOMContentLoaded", function () {
   changeActiveLink();
   window.addEventListener("scroll", changeActiveLink);
 });
+
+// Story
+const episodes = {
+  1: {
+    title: "Episode 1: 魔女見習いイレイナ",
+    synopsis:
+      'After reading "The Adventures of Nike," Elaina dreams of becoming a witch and traveling. She passes the exam, and all that\'s left to do is become an apprentice to another witch to become a full-fledged witch, but she is turned away by every witch. Finally, she decides to ask the suspicious "Stardust Witch" to become her apprentice.',
+    video: "https://www.youtube.com/embed/3zStBd6qVbw",
+  },
+  2: {
+    title: "Episode 2: Perjalanan Baru",
+    synopsis: "Ini adalah sinopsis untuk episode 2.",
+    video: "https://www.youtube.com/embed/example2",
+  },
+  3: {
+    title: "Episode 3: Petualangan Ajaib",
+    synopsis: "Ini adalah sinopsis untuk episode 3.",
+    video: "https://www.youtube.com/embed/example3",
+  },
+};
